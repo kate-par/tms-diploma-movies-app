6eab498bd08b1d92d1e724f72803da2c8389586f
kate-par <katyaparkhamovich@gmail.com>
2021-12-01 04:05:16 +0300

    feat(components):create header,search,topbar,footer

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Search.module.css";
+
+const Search = () => {
+  return (
+    <div className={styles.firstscreen}>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
+export default Search;
---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./TopBar.module.css";
+
+const TopBar = () => {
+  return (
+    <div className={styles.component}>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
+export default TopBar;
