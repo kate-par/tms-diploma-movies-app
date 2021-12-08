35189f5674b3fb3590be59b5d8e31f86aa77ff16
kate-par <katyaparkhamovich@gmail.com>
2021-12-09 00:24:11 +0300

    feat(app):add router and NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,70 @@
+// import React from "react";
+// import styles from "./App.module.css";
+// import Header from "./components/Header";
+// import Search from "./components/Search";
+// import Footer from "components/Footer";
+
+// function App() {
+//   return (
+//     <div className={styles.app}>
+//       <div className={styles.wrap}>
+//         <Header />
+//         <Search />
+//       </div>
+//       <div className={styles.footer}>
+//         <Footer />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import styles from "./App.module.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Footer from "components/Footer";
+import NotFound from "components/NotFound";
 
 function App() {
   return (
-    <div className={styles.app}>
-      <div className={styles.wrap}>
-        <Header />
-        <Search />
+    <Router>
+      <div className={styles.app}>
+        <Routes>
+          <Route
+            path="/movies"
+            element={
+              <>
+                <div className={styles.wrap}>
+                  <Header />
+                  <Search />
+                </div>
+                <div className={styles.footer}>
+                  <Footer />
+                </div>
+              </>
+            }
+          ></Route>
+          <Route
+            path="/not-found"
+            element={
+              <div className={styles.container}>
+                <NotFound />
+              </div>
+            }
+          ></Route>
+          <Route path="/" element={<Navigate to="/movies" />}></Route>
+          <Route path="*" element={<Navigate to="/not-found" />}></Route>
+        </Routes>
       </div>
-      <div className={styles.footer}>
-        <Footer />
-      </div>
-    </div>
+    </Router>
   );
 }
 
