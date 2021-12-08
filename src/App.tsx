3bec21bfa69e6cfdea3ed11e4e5df356ee9f9faa
kate-par <katyaparkhamovich@gmail.com>
2021-12-09 00:54:33 +0300

    fix(app):no significant changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import styles from "./App.module.css";
-// import Header from "./components/Header";
-// import Search from "./components/Search";
-// import Footer from "components/Footer";
-
-// function App() {
-//   return (
-//     <div className={styles.app}>
-//       <div className={styles.wrap}>
-//         <Header />
-//         <Search />
-//       </div>
-//       <div className={styles.footer}>
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import {
   BrowserRouter as Router,
